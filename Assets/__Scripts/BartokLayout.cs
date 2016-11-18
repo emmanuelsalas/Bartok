2ed13c60fb452acd089dec508220dcaaccd620ae
emmanuelsalas <eas282@scarletmail.rutgers.edu>
2016-11-17 19:01:32 -0500

    Stuck no.2
    p.581. Stuck for several reasons

---
+++ Assets/__Scripts/BartokLayout.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SlotDef{
+	public float x;
+	public float y;
+	public bool faceUp=false;
+	public string layerName="Defualt";
+	public int layerID=0;
+	public int id;
+	public List<int> hiddenBy=new List<int>();
+	public float rot;
+	public string type = "slot";
+	public Vector2 stagger;
+	public int player;
+	public Vector3 pos;
+}
+
+public class BartokLayout : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
